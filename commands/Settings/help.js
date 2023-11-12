4227d12077213213c99e4110de1132926ffce083
mrcraked <89685779+mrcraked@users.noreply.github.com>
2023-11-12 05:58:18 +0000

    GYAT

--- commands/Settings/help.js
+++ commands/Settings/help.js
@@ -7,13 +7,21 @@ module.exports = {
     .setDescription('Lists all available commands'),
     async execute(interaction) {
         let str
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+        const foldersPath = path.join(__dirname, 'commands');
+        const commandFolders = fs.readdirSync(foldersPath);
 
-        for (const file of commandFiles) {
-        const command = require(`./${file}`);
-        str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
-        }
-
+        for (const folder of commandFolders) {
+          // Grab all the command files from the commands directory you created earlier
+          const commandsPath = path.join(foldersPath, folder);
+          const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+          // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+          for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(`./${file}`);
+            str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
+            } 
+          }
+      
         return interaction.reply({
         content: str,
         ephemeral: true,
